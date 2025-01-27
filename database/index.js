691e217525ec1f1a65abd83cbf0b4770b55c0d7e
Ernest <ooquirrino@gmail.com>
2025-01-27 21:42:40 +0300

    fix db connection

--- database/index.js
+++ database/index.js
@@ -7,32 +7,32 @@ require("dotenv").config()
  * If - else will make determination which to use
  * *************** */
 let pool
-if (process.env.NODE_ENV == "development") {
-    pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: true
-          //rejectUnauthorized: false,
-        },
-    })
+// if (process.env.NODE_ENV == "development") {
+//     pool = new Pool({
+//         connectionString: process.env.DATABASE_URL,
+//         ssl: {
+//           rejectUnauthorized: true
+//           //rejectUnauthorized: false,
+//         },
+//     })
 
-    // Added for troubleshooting queries
-    // during development
-    module.exports = {
-        async query(text, params) {
-            try {
-            const res = await pool.query(text, params)
-            console.log("executed query", { text })
-            return res
-            } catch (error) {
-            console.error("error in query", { text })
-            throw error
-            }
-        },
-    }
-} else {
+//     // Added for troubleshooting queries
+//     // during development
+//     module.exports = {
+//         async query(text, params) {
+//             try {
+//             const res = await pool.query(text, params)
+//             console.log("executed query", { text })
+//             return res
+//             } catch (error) {
+//             console.error("error in query", { text })
+//             throw error
+//             }
+//         },
+//     }
+// } else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
   })
   module.exports = pool
-}
+//}
