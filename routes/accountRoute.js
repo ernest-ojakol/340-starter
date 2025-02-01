082396e15b2d87d23026b9e3828d11ab6a01361b
Ernest <ooquirrino@gmail.com>
2025-02-01 23:55:25 +0300

    week 04 Practice

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,7 +2,10 @@
 const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
+const utilities = require("../utilities/")
 
 router.get("/account/login", accountController.buildLogin);
+router.get("/account/registerr", accountController.buildRegister)
+router.post('/account/registerr', utilities.handleErrors(accountController.registerAccount))
 
 module.exports = router;
