e9628ca0b343bffc42fbb0d08b0d172e33311434
Ernest <ooquirrino@gmail.com>
2025-02-02 23:40:13 +0300

    render errors

--- server.js
+++ server.js
@@ -39,7 +39,6 @@ app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
